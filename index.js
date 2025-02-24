40bdbebb17d2fd4ba8042fcc266c0c293cb5f0c9
Joshuaaa124 <132545246+Joshuaaa124@users.noreply.github.com>
2025-02-25 00:19:46 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    document.getElementById('popupOverlay').style.display = 'block';
+    document.getElementById('popupBox').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popupOverlay').style.display = 'none';
+    document.getElementById('popupBox').style.display = 'none';
+}
